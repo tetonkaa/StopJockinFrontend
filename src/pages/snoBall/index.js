e0a5716f08ccf668dde6a14fb864b60de90a9bf9
Angel <angel.l.puente@gmail.com>
2023-03-07 17:35:31 -0600

    update snoBall image collage

--- src/pages/snoBall/index.js
+++ src/pages/snoBall/index.js
@@ -8,9 +8,12 @@ import React, { useState } from 'react';
 //image imports
 import crawFries from './snoBallphotos/crawFishFries.jpg'
 import friedShrimp from './snoBallphotos/friedShrimp.jpg'
+import shrimpFries from './snoBallphotos/somefriesthing1.PNG'
+import crawFishNachos from './snoBallphotos/crawFishNachos.jpg'
 import snowBall1 from './snoBallphotos/snowBall1.jpg'
 import snowBall2 from './snoBallphotos/snowball2.jpg'
 import snowBall3 from './snoBallphotos/snowball3.jpg'
+import snowBall4 from './snoBallphotos/snowball4.jpg'
 
 
 function ControlledCarousel() {
@@ -19,6 +22,13 @@ function ControlledCarousel() {
     const handleSelect = (selectedIndex, e) => {
       setIndex(selectedIndex);
     };
+
+    const [hover1, setOver] = useState(false);
+    const [hover2, setOver2] = useState(false);
+    const [hover3, setOver3] = useState(false);
+    const [hover4, setOver4] = useState(false);
+
+    
   
     return (
       <Carousel className="snoballCarousel" activeIndex={index} onSelect={handleSelect} indicators={false}>
@@ -80,21 +90,46 @@ function ControlledCarousel() {
         </Carousel.Item>
         <Carousel.Item interval={950000}>
 
-          <div class="row">
-
+          <div class="foodImagesContainer">
+          <h1 className="snowballMenu">Media</h1>
             <div class="column">
-              <img src={crawFries}/>
-              <img src={friedShrimp}/>
-              <img src={friedShrimp}/>
+            <img onMouseOver={() => setOver3(true)}
+                    onMouseOut={() => setOver3(false)} 
+                    src={hover3 ? snowBall1 : snowBall2}
+                        alt="snowBall2"/>
+              <img onMouseOver={() => setOver2(true)}
+                    onMouseOut={() => setOver2(false)} 
+                    src={hover2 ? crawFishNachos : friedShrimp}
+                        alt="crawFries"/>
+              
               
             </div>
 
             <div class="column">
-              <img src={snowBall1}/>
-              <img src={snowBall2}/>
-              <img src={snowBall3}/>
+            
+              <img onMouseOver={() => setOver(true)}
+                    onMouseOut={() => setOver(false)} 
+                    src={hover1 ? shrimpFries : crawFries}
+                        alt="crawFries"/>
+              <img onMouseOver={() => setOver4(true)}
+                    onMouseOut={() => setOver4(false)} 
+                    src={hover4 ? snowBall3 : snowBall4}
+                        alt="crawFries"/>
+              
 
             </div>
+            {/* <div class="HiddenColumn">
+              <img src={snowBall3}/>
+              <img src={snowBall3}/>
+              
+              
+            </div>
+            <div class=" HiddenColumn">
+              <img src={snowBall3}/>
+              <img src={snowBall3}/>
+              
+              
+            </div> */}
 
           </div>
 
@@ -102,12 +137,7 @@ function ControlledCarousel() {
 
         </Carousel.Item>
         <Carousel.Item interval={950000}>
-          <img
-            src="https://i.imgur.com/IGIEO39.jpeg"
-            alt="Third slide"
-          />
-  
-
+          
         </Carousel.Item>
       </Carousel>
     );
